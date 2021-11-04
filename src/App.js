c1e37caf78c319ca0e4834716a0a405dd285fe36
Hernan Pinol <hernanpinol@hotmail.com>
2021-11-03 22:12:08 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>Museo Histórico de la Colonia San José</h1>
+    </div>
+  );
+}
+
+export default App;
